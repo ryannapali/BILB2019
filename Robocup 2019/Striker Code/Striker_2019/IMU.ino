6af7dc0628eb06978928f9d0eafae7ebfdd01a0e
Jesse <jesse.gallant4@gmail.com>
2019-04-18 13:29:42 -0400

    did sum stuff

--- Robocup 2019/Striker Code/Striker_2019/IMU.ino
+++ Robocup 2019/Striker Code/Striker_2019/IMU.ino
@@ -51,11 +51,7 @@ float IMU_calcError(float targetAngle) {
    Returns if facing goal
 */
 boolean IMU_atGoal() {
-  IMU_GetReadings();
-  if (abs(IMU_calcError(g_goal)) < 30) {
-    return true;
-  }
-  return false;
+
 }
 
 /*
@@ -70,11 +66,7 @@ void IMU_checkIfResetGoal() {
 }
 
 void IMU_spinToDirection(float targetDirection) {
-  int k = 2;
-  while (abs(IMU_calcError(targetDirection)) > 10) {
-    spin(IMU_calcError(targetDirection) * k);
-  }
-  stopMotors();
+
 }
 
 /*
@@ -85,26 +77,26 @@ void IMU_spinToDirection(float targetDirection) {
 */
 
 void driveToHeadingIMU(float facing, float angle, float speed) {
-  int k = 1.5;
-  IMU_GetReadings();
-  float facingError = IMU_calcError(facing);
-  float rad = getRad(angle);
-  float proportionals[] = {sin(-rad + 3.92699082), sin(-rad + 5.28834763), sin(-rad + 0.994837674), sin(-rad + 2.35619449)};
-
-  setM1Speed(speed * proportionals[0] - (facingError)*k);
-  setM2Speed(speed * proportionals[1] - (facingError)*k);
-  setM3Speed(speed * proportionals[2] - (facingError)*k);
-  setM4Speed(speed * proportionals[3] - (facingError)*k);
+//  int k = 1.5;
+//  IMU_GetReadings();
+//  float facingError = IMU_calcError(facing);
+//  float rad = getRad(angle);
+//  float proportionals[] = {sin(-rad + 3.92699082), sin(-rad + 5.28834763), sin(-rad + 0.994837674), sin(-rad + 2.35619449)};
+//
+//  setM1Speed(speed * proportionals[0] - (facingError)*k);
+//  setM2Speed(speed * proportionals[1] - (facingError)*k);
+//  setM3Speed(speed * proportionals[2] - (facingError)*k);
+//  setM4Speed(speed * proportionals[3] - (facingError)*k);
 }
 
 void spinSlowCheckPossesion(float targetDirection) {
-    int k = 2;
-    while (abs(IMU_calcError(targetDirection)) > 10 && checkPossession() == true) {
-      if (IMU_calcError(targetDirection)*k > 70) spin(70); //sets a max and min speed it can turn at
-      else if (IMU_calcError(targetDirection)*k < -70) spin(-70);
-      else spin(IMU_calcError(targetDirection)*k);
-    }
-    stopMotors();
-    //clearCameraBuffer(); COMMENT THIS BACK IN
+//    int k = 2;
+//    while (abs(IMU_calcError(targetDirection)) > 10 && checkPossession() == true) {
+//      if (IMU_calcError(targetDirection)*k > 70) spin(70); //sets a max and min speed it can turn at
+//      else if (IMU_calcError(targetDirection)*k < -70) spin(-70);
+//      else spin(IMU_calcError(targetDirection)*k);
+//    }
+//    stopMotors();
+//    //clearCameraBuffer(); COMMENT THIS BACK IN
   }
 
